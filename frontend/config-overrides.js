e4d21900efd8374c203420265ab77ddfae61f2c4
Iamthegreatestofalltime <alexlotkov124@gmail.com>
2023-09-24 15:31:09 -0400

    heroku commit again

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,23 +1,20 @@
-const { override, addWebpackAlias } = require('customize-cra');
 const path = require('path');
 const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-    addWebpackAlias({
-        'crypto': path.resolve(__dirname, 'node_modules/crypto-browserify'),
-        'stream': path.resolve(__dirname, 'node_modules/stream-browserify'),
-        'buffer': path.resolve(__dirname, 'node_modules/buffer'),
-        'util': path.resolve(__dirname, 'node_modules/util')
-    })
+    // Alias configuration
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        'crypto': path.resolve(__dirname, '../backend/node_modules/crypto-browserify'),
+        'stream': path.resolve(__dirname, '../backend/node_modules/stream-browserify'),
+        'buffer': path.resolve(__dirname, '../backend/node_modules/buffer'),
+        'util': path.resolve(__dirname, '../backend/node_modules/util')
+    };
+
+    // Providing process polyfill
     config.plugins.push(new webpack.ProvidePlugin({
         process: 'process/browser',
     }));
-
-    config.resolve.fallback = {
-        ...config.resolve.fallback, // This ensures you don't accidentally override any existing fallbacks
-        "crypto": require.resolve("crypto-browserify"),
-        "stream": require.resolve("stream-browserify"),
-    };
 
     return config;
 };
